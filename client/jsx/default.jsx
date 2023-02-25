9fac3b23ee9bc8c017bf8e1b6a4964407d1022c4
INS140 <IsaacNSaettel@gmail.com>
2023-02-25 03:13:06 -0800

    initial

---
+++ client/jsx/default.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Default({ children }) {
+  return (
+    <html lang='en'>
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="styles/main.css" />
+        <title>INS140 | Interactive comments section</title>
+      </head>
+      <body>
+        {children}
+        <footer class="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+          Coded by <a href="https://github.com/INS140">INS140</a>.
+        </footer>
+      </body>
+    </html>
+  )
+}
---
+++ client/jsx/index.jsx
@@ -0,0 +1,5 @@
+import React from "react"
+import { hydrateRoot } from "react-dom/client"
+import { Default } from "./default"
+
+hydrateRoot(document, <Default />)
